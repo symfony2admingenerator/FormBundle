bb19bb6562cdba7e027a1268fb79e1fb60c1185d
Tobias Feijten <tobias93@live.nl>
2025-12-02 19:38:20 +0100

    Fix service argument definition

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -3,6 +3,7 @@
 use Admingenerator\FormBundle\Twig\Extension\FormExtension;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return function (ContainerConfigurator $container): void {
     $container->parameters()->set('admingenerator.twig.extension.form.class', FormExtension::class);
@@ -10,5 +11,5 @@ return function (ContainerConfigurator $container): void {
     $container->services()
         ->set('admingenerator.twig.extension.form', param('admingenerator.twig.extension.form.class'))
         ->tag('twig.extension')
-        ->arg('$renderer', 'twig.form.renderer');
+        ->arg('$renderer', service('twig.form.renderer'));
 };
